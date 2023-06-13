1b8c7dbf6098746b4d2a1ff14025b04a8abd3030
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-06-13 12:47:58 +0300

    16

--- tests/trees/test_max_tree.py
+++ tests/trees/test_max_tree.py
@@ -24,3 +24,15 @@ def test_max_root_left_right():
     root_node.right = Node(-1)
     tree.root = root_node
     assert tree.max_node() == -1
+
+def test_max_complex_tree():
+    tree = BinaryTree()
+    root_node = Node(10)
+    root_node.left = Node(5)
+    root_node.right = Node(1)
+    root_node.right.right = Node(20)
+    root_node.right.right = Node(30)
+    root_node.left.right = Node(15)
+    root_node.left.right = Node(16)
+    tree.root = root_node
+    assert tree.max_node() == 30
