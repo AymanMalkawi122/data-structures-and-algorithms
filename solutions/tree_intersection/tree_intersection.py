ca91525346fd8440aea85e241ee8974d6d2b9b84
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-08-01 02:33:08 +0300

    .

--- solutions/tree_intersection/tree_intersection.py
+++ solutions/tree_intersection/tree_intersection.py
@@ -1,12 +1,32 @@
 from solutions.hash_tables.solution import Hashtable
 def traverse(node, result):
+  """
+    Perform a pre-order traversal of a binary tree starting from the given 'node'.
+
+    Args:
+        node (TreeNode): The starting node of the binary tree.
+        result (list): A list to store the values obtained from the traversal.
+
+    Returns:
+        None
+  """
   if node:
     result.append(node.value)
     traverse(node.left, result)
     traverse(node.right, result)
-    
+   
 
 def tree_intersection(tree1, tree2):
+    """
+    Find the common values that occur in both binary trees 'tree1' and 'tree2'.
+
+    Args:
+        tree1 (BinaryTree): The first binary tree.
+        tree2 (BinaryTree): The second binary tree.
+
+    Returns:
+        list: A list containing values common to both binary trees.
+    """
     repetitions = Hashtable(100)
     tree_values = []
     traverse(tree1.root, tree_values)
